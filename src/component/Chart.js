010d25c0a13b4df90970ba53fe1ecb3080e6ad64
pwilcz <przemwil@gmail.com>
2019-11-01 16:51:36 +0100

    Add description to chart toolkit

--- src/component/Chart.js
+++ src/component/Chart.js
@@ -8,14 +8,14 @@ function Chart(props) {
             <ResponsiveContainer width="100%" height={280}>
                 <LineChart data={props.data}  margin={{ top: 5, right: 5, left: 5, bottom: 35 }}>
                     <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey={"x"} domain={[0, 1]} type="number">
+                    <XAxis dataKey={"x"} domain={[0, 1]} type="number" name={props.xLabel}>
                         <Label value={props.xLabel} offset={-5} position="insideBottom" />
                     </XAxis>
                     <YAxis domain={[0, 1]} type="number">
                         <Label value={props.yLabel} angle={-90} position="insideLeft" />
                     </YAxis>
-                    <Tooltip />
-                    <Line type="linear"  dataKey="y"  stroke="#874BBA" strokeWidth="2" />
+                    <Tooltip labelFormatter={(label) => props.xLabel + ": " + label}/>
+                    <Line type="linear"  dataKey="y"  stroke="#874BBA" strokeWidth="2" name={props.yLabel} />
                 </LineChart>
             </ResponsiveContainer>
         </div>
