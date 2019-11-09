6a198e956c6d2e4e8258e4225aa6bad67151a5c0
pwilcz <przemwil@gmail.com>
2019-11-09 16:17:03 +0100

    production build

---
+++ build/precache-manifest.7e3688c9971615f994774b6aec5155a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2676e43f3d1defe5bdf56a8aba8741",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1afcd2e4e25f8d1d11e7",
+    "url": "./static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9ef761125ec126334b29",
+    "url": "./static/css/main.1ee89ef3.chunk.css"
+  },
+  {
+    "revision": "1afcd2e4e25f8d1d11e7",
+    "url": "./static/js/2.bc4f042f.chunk.js"
+  },
+  {
+    "revision": "9ef761125ec126334b29",
+    "url": "./static/js/main.eb7abcf2.chunk.js"
+  },
+  {
+    "revision": "e0cd4f78a7abc015e65c",
+    "url": "./static/js/runtime-main.931418c0.js"
+  }
+]);
